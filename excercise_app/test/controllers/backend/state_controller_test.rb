33618291412d3c14ce083c4d4306f0a4b175c0e0
mohameda <mygithub.work@gmail.com>
2014-10-13 16:26:53 +0800

    TODAY EXCERCISE COMPLETED

---
+++ excercise_app/test/controllers/backend/state_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Backend::StateControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ excercise_app/test/helpers/backend/state_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Backend::StateHelperTest < ActionView::TestCase
+end
